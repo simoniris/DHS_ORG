7d911109a040d83de5efaf74499164aeda1ed2a5
vinsilin <vinsilin@irissoftek.com>
2017-03-22 13:16:47 +0530

    commit

--- addlayer.php
+++ addlayer.php
@@ -16,7 +16,7 @@
 
 	$USERNAME=$user->get('username');
 
-	echo $user = $session->get( 'user' );
+	 $user = $session->get( 'user' );
 
 
 	$namequery="select concat(name,' ',middle_name,' ',last_name)as name from ".$prefix."users where id='".$ID."'";
